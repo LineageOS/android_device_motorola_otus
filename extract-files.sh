b1c2ad8a00172b881b9bb38a504c77d9df214ea3
Matthias Yzusqui <myzb.dev@gmail.com>
2017-08-15 22:48:02 -0400

    otus: Update extraction scripts
    * Kanged from titan
    * Meant to work with updated scripts in common
    
    Change-Id: Ifc6411469323258b745bc6051354a4b5ac5a0bf1

--- extract-files.sh
+++ extract-files.sh
@@ -1,33 +1,27 @@
-#!/bin/sh
+#!/bin/bash
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-VENDOR=motorola
-DEVICE=otus
+set -e
 
-BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+# Required!
+export DEVICE=otus
+export DEVICE_COMMON=msm8610-common
+export VENDOR=motorola
 
-while getopts ":nh" options
-do
-  case $options in
-    n ) NC=1 ;;
-    h ) echo "Usage: `basename $0` [OPTIONS] "
-        echo "  -n  No clenup"
-        echo "  -h  Show this help"
-        exit ;;
-    * ) ;;
-  esac
-done
+export DEVICE_BRINGUP_YEAR=2015
 
-if [ "x$NC" != "x1" ];
-then
-    rm -rf $BASE/*
-fi
-
-for FILE in `cat proprietary-blobs.txt | grep -v ^# | grep -v ^$`; do
-    DIR=`dirname $FILE`
-    if [ ! -d $BASE/$DIR ]; then
-        mkdir -p $BASE/$DIR
-    fi
-    adb pull /system/$FILE $BASE/$FILE
-done
-
-./setup-makefiles.sh
+./../../$VENDOR/$DEVICE_COMMON/extract-files.sh $@
